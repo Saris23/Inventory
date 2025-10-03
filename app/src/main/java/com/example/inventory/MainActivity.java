18708c14471e0864562a746b49c388ab03e4c058
sara <saritagarcia931@gmail.com>
2025-10-02 21:11:02 -0500

    menu en MainActivity - se crea carpeta de menu y un xml de menu con items

--- app/src/main/java/com/example/inventory/MainActivity.java
+++ app/src/main/java/com/example/inventory/MainActivity.java
@@ -1,6 +1,11 @@
 package com.example.inventory;
 
+import android.content.Intent;
+import android.media.Image;
 import android.os.Bundle;
+import android.view.MenuItem;
+import android.widget.ImageButton;
+import android.widget.PopupMenu;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -19,6 +24,48 @@ public class MainActivity extends AppCompatActivity {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
+        });
+        ImageButton btnMenu = findViewById(R.id.btnMenu);
+
+        btnMenu.setOnClickListener(view ->{
+            PopupMenu popupMenu = new PopupMenu(MainActivity.this, view);
+            popupMenu.getMenuInflater().inflate(R.menu.menu,popupMenu.getMenu());
+            popupMenu.setOnMenuItemClickListener(item -> {
+                int id = item.getItemId();
+                if (id == R.id.itInicio) {
+                    Intent inicio = new Intent(MainActivity.this, MainActivity.class);
+                    startActivity(inicio);
+                    finish();
+                    return true;
+
+                } else if (id == R.id.itStock) {
+                    Intent stock = new Intent(MainActivity.this, stock.class);
+                    startActivity(stock);
+                    finish();
+                    return true;
+
+                } else if (id == R.id.itPerfil) {
+                    Intent perfil = new Intent(MainActivity.this, perfil.class);
+                    startActivity(perfil);
+                    finish();
+                    return true;
+
+                } else if (id == R.id.itHistorial) {
+                    Intent history = new Intent(MainActivity.this, historial.class);
+                    startActivity(history);
+                    finish();
+                    return true;
+
+                } else if (id == R.id.itLogout) {
+                    Intent logout = new Intent(MainActivity.this, startpage.class);
+                    startActivity(logout);
+                    finish();
+                    return true;
+                }
+
+                return false;
+            });
+            popupMenu.show();
         });
     }
 }
--- app/src/main/java/com/example/inventory/stock.java
+++ app/src/main/java/com/example/inventory/stock.java
@@ -1,8 +1,6 @@
 package com.example.inventory;
 
-import android.content.Intent;
 import android.os.Bundle;
-import android.widget.Button;
 
 import androidx.activity.EdgeToEdge;
 import androidx.appcompat.app.AppCompatActivity;
@@ -10,30 +8,17 @@ import androidx.core.graphics.Insets;
 import androidx.core.view.ViewCompat;
 import androidx.core.view.WindowInsetsCompat;
 
-public class login extends AppCompatActivity {
+public class stock extends AppCompatActivity {
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         EdgeToEdge.enable(this);
-        setContentView(R.layout.activity_login);
+        setContentView(R.layout.activity_stock);
         ViewCompat.setOnApplyWindowInsetsListener(findViewById(R.id.main), (v, insets) -> {
             Insets systemBars = insets.getInsets(WindowInsetsCompat.Type.systemBars());
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom);
             return insets;
-        });
-        Button btnIngresar = findViewById(R.id.btnIngresar);
-        Button btnRegistro = findViewById(R.id.btnRegistro);
-
-        btnIngresar.setOnClickListener(view ->{
-            Intent intent = new Intent(login.this, MainActivity.class);
-            startActivity(intent);
-            finish();
-        });
-        btnRegistro.setOnClickListener(view ->{
-            Intent intentR = new Intent(login.this, register.class);
-            startActivity(intentR);
-            finish();
         });
     }
 }
